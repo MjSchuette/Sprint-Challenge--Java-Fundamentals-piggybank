2260f2c2b76222a17f6caafda961d9b06200a934
MatthewSchuette <mjschuette4@gmail.com>
2020-07-10 11:03:29 -0500

    Endpoint is working, now have to get it to display correct information

--- piggybank/src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
+++ piggybank/src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -1,4 +1,27 @@
 package com.lambdaschool.piggybank.controllers;
 
+import com.lambdaschool.piggybank.models.Coin;
+import com.lambdaschool.piggybank.repositories.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CoinController {
+    @Autowired
+    CoinRepository coinrepos;
+
+    @GetMapping(value = "/total", produces = {"application/json"})
+    public ResponseEntity<?> coinList(){
+        List<Coin> myList = new ArrayList<>();
+        coinrepos.findAll().iterator().forEachRemaining(myList::add);
+
+        myList.sort((e1, e2) -> e1.getName().compareToIgnoreCase(e2.getName()));
+        return new ResponseEntity<>(myList, HttpStatus.OK);
+    }
 }
